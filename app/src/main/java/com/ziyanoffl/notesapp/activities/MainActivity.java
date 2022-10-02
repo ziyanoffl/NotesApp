e6e4c315fe6641e8b21e80cb6ff74bff9069e509
ziyanoffl <ziyan.offl@gmail.com>
2022-10-02 21:34:45 +0530

    Version 1.4
    App UI updated to show the notes in recyclerview. Also some more changes in layouts.

--- app/src/main/java/com/ziyanoffl/notesapp/activities/MainActivity.java
+++ app/src/main/java/com/ziyanoffl/notesapp/activities/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ziyanoffl.notesapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,14 +14,20 @@ import android.widget.ImageView;
 
 import com.ziyanoffl.notesapp.R;
 import com.ziyanoffl.notesapp.activities.CreateNoteActivity;
+import com.ziyanoffl.notesapp.adapters.NotesAdapter;
 import com.ziyanoffl.notesapp.database.NotesDatabase;
 import com.ziyanoffl.notesapp.entities.Note;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE_ADD_INPUT = 1;
+
+    private RecyclerView notesRecyclerView;
+    private List<Note> noteList;
+    private NotesAdapter notesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +43,20 @@ public class MainActivity extends AppCompatActivity {
                 );
             }
         });
+
+        notesRecyclerView = findViewById(R.id.notesRecyclerView);
+        notesRecyclerView.setLayoutManager(
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        );
+        noteList = new ArrayList<>();
+        notesAdapter = new NotesAdapter(noteList);
+        notesRecyclerView.setAdapter(notesAdapter);
+
         getNotes();
     }
 
     private void getNotes(){
+        @SuppressLint("StaticFieldLeak")
         class GetNotesTask extends AsyncTask<Void, Void, List<Note>>{
             @Override
             protected List<Note> doInBackground(Void... voids) {
@@ -46,10 +65,18 @@ public class MainActivity extends AppCompatActivity {
                         .noteDao().getAllNotes();
             }
 
+            @SuppressLint("NotifyDataSetChanged")
             @Override
             protected void onPostExecute(List<Note> notes) {
                 super.onPostExecute(notes);
-                Log.d("MY_NOTES", notes.toString());
+                if(noteList.size() == 0){
+                    noteList.addAll(notes);
+                    notesAdapter.notifyDataSetChanged();
+                }else{
+                    noteList.add(0, notes.get(0));
+                    notesAdapter.notifyItemInserted(0);
+                }
+                notesRecyclerView.smoothScrollToPosition(0);
             }
         }
         new GetNotesTask().execute();
